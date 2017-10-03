336ea742ec698a41c1fbfe615ce386c89a78dc0e
xdkappa <charlescarino4@gmail.com>
2017-10-04 07:50:10 +0800

    LOL FIXES. :D

--- cia/assessment.php
+++ cia/assessment.php
@@ -1,45 +1,342 @@
-<?php
-class Assessment{
-
-    function __construct(){
-    }
-
-    // function getAnswerKeys($db){
-    //     return $db->select()->from('collegeanskey')->fetch();
-    // }
-    //
-    // function getCollegeQuestionAnswerKeys($db){
-    //   return $db->select('a.anskeyID anskeyID,c.collegeName collegeName,q.questionID questionID,q.questionText questionName,a.answer answer')
-    //   ->from('collegeanskey a')->join('college c','c.collegeID = a.collegeID','left')->join('question q','q.questionID = a.questionID','left')
-    //   ->order_by('questionID asc,collegeName asc')->fetch();
-    // }
-    //
-    // function addAnswerKey($db,$data){
-    //     // $db->select()->from("collegeanskey")->where('collegeID',$data['collegeID'])->execute();
-    //     // if (($db->affected_rows)<1) {
-    //         $id = $db->insert("collegeanskey",$data);//returns the last id inserted
-    //     // }else{
-    //     //     $id = 0;
-    //     // }
-    //     return $id;
-    // }
-    //
-    // function editAnswerKey($db,$id,$values){
-    //     $db->where('ansKeyID',$id)->update('collegeanskey',$values);
-    //
-    //     return $id;
-    // }
-    //
-    // function deleteAnswerKey($db,$id){
-    //     $db->delete("collegeanskey")->where('ansKeyID',$id)->execute();
-    //
-    //     return $id;
-    // }
-    //
-    // function getAnswerKey($db,$id){
-    //     return $db->select()->from('collegeanskey')->where('ansKeyID',$id)->fetch();
-    // }
-
-    function processAssessment(){
-    }
-}
+<?php include "header.php"; ?>
+<!-- Page Content -->
+<link rel="stylesheet" type="text/css" href="css/assessment-styles.css">
+<section id="services-1" class="section-padding wow fadeInUp delay-05s">
+    <div class="container">
+        <div id="quizzie">
+          <h1>College Inquiry Assessment Test</h1>
+          <hr class="bottom-line">
+          <div class="question-div">
+            <ul class="quiz-step step1 current">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">Your degree has you use computers on a daily basis</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step2">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">Your degree requires you to have financial literacy.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step3">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">Your degree is socially based.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step4">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">The word "enthusiastic" describes your degree.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step5">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">You have an interest in the study of people's culture.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step6">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">You love math.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step7">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">You are interested in the human anatomy.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step8">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">You love to teach or help others.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step9">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">You are confident at public speaking.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul class="quiz-step step10">
+                <li class="question">
+                    <div class="question-wrap">
+                        <h2 class="q-a">Your ideal career will most likely be outdoors.</h2>
+                    </div>
+                </li>
+                  <li class="quiz-answer low-value" data-quizIndex="2">
+                      <div class="answer-wrap">
+                          <p class="answer-text">0</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer high-value" data-quizIndex="4">
+                      <div class="answer-wrap">
+                          <p class="answer-text">1</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 2-value" data-quizIndex="6">
+                      <div class="answer-wrap">
+                          <p class="answer-text">2</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 3-value" data-quizIndex="8">
+                      <div class="answer-wrap">
+                          <p class="answer-text">3</p>
+                      </div>
+                  </li>
+                  <li class="quiz-answer 4-value" data-quizIndex="10">
+                      <div class="answer-wrap">
+                          <p class="answer-text">4</p>
+                      </div>
+                  </li>
+            </ul>
+            <ul id="results">
+                <li class="results-inner">
+                    <p>Your result is:</p>
+                    <h1></h1>
+                    <p class="desc"></p>
+                </li>
+            </ul>
+          </div>
+        </div>
+    </div>
+  </section>
+<!-- /.container -->
+<?php include "footer.php"; ?>
