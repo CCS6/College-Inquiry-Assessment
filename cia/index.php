b2f50c0c41bfc518106c01bad140fceac69f777f
KoyaKuya <30855417+KuyaKoya@users.noreply.github.com>
2017-10-11 10:41:26 +0800

    changes in the index

--- cia/index.php
+++ cia/index.php
@@ -1,4 +1,12 @@
-<?php include "header.php";?>
+<!-- test -->
+<?php
+include "header.php";
+include 'config.php';
+include "classes/Colleges.php";
+$c = new Colleges();
+$result = $c->getColleges($db);
+?>
+
         <div class="wrapper">
         <div class="container">
           <div class="row">
@@ -16,6 +24,7 @@
     </div>
     <!--/ HEADER-->
     <!---->
+
     <section id="service" class="section-padding wow fadeInUp delay-05s">
       <div class="container">
         <div class="row">
@@ -23,32 +32,37 @@
             <h2 class="service-title pad-bt15">Colleges</h2>
             <hr class="bottom-line">
           </div>
-          <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="service-item">
-              <h3><span>C</span>ollege of Agriculture</h3>
-              <p>The College of Agriculture emphasizes academic preparation with extensive community practice for leadership in rural Philippines. Courses are geared to understanding our society and to developing skills needed for making agricultural communities productive and financially viable.</p>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="service-item">
-              <h3><span>C</span>ollege of Arts and Sciences</h3>
-              <p>Granted Level III accreditation status by recognized accrediting agencies in the Philippine educational system, the College provides undergraduate and graduate instruction in various areas of learning such as in the fields of Anthropology,Creative Writing, English Language, Filipino, History, Literature, Philosophy, Political Science, Sociology, Physics, Chemistry, Biology, Mathematics among others.</p>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="service-item">
-              <h3><span>C</span>ollege of Business Administration</h3>
-              <p>Within the framework of the mission statement of Silliman University, the College of Business Administration (CBA) aims to inculcate in the students the philosophy of service to others, nurtured by the ideals of Christian faith as it prepares them for responsible leadership in agriculture, commerce, industry, and government.</p>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="service-item">
-              <h3><span>C</span>ollege of Computer Studies</h3>
-              <p>In line with the Commission on Higher Education’s thrust to control the number of Information Technology Education degrees and to centralize its offering within the institution, Silliman University created the College of Information Technology and Computer Sciences.</p>
-            </div>
-          </div>
-      </div>
-      </div>
+
+<!-- query for college -->
+          <?php
+          $rowOpen = '';
+          $rowClose = '';
+          $i = 1;
+          $x = 1;
+          foreach($result as $key) {
+              ?>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="service-item">
+                  <h3><span><?= substr($key['collegeName'], 0, 1) ?></span><?= substr($key['collegeName'], 1) ?></h3>
+                 <p><?= $c->get_words($key['collegeAboutInfo'])?>...</p>
+                </div>
+              </div>
+              <?php
+              if(($i%4) == 0){
+                  $rowClose = '</div>';
+                 $rowOpen = '<div class="row">';
+                  echo $rowClose;
+                  echo $rowOpen;
+              }
+
+                  if($x == 4){
+                    break;
+                  }
+                  $i++;
+                  $x++;
+              }
+              ?>
+
       <div id="more-colleges"class="container">
           <a class="a-colleges" href="colleges.php">click here to learn more...</a>
       </div>
@@ -64,34 +78,37 @@
                 <img class="circle-profile"src="img/20819343_851506305010941_8992152039636009533_o.jpg">
               </div>
               <h3 class="pad-bt15">Derick Ho</h3>
-              <p>Born April 28, 1994. Currently studying computer science at the College of Computer Studies of Silliman University. Derick was the original invisionary of this project which is why he was assigned as the manager of the team.</p>
+              <p>"The universe is now in the short life given to me in the time created by man, provided by God." <br>- Derick</p>
             </div>
           </div>
-          <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="wrap-item text-center">
-              <div class="item-img">
-                <img class="circle-profile"src="img/13006598_999938766759664_6860126981732108419_n.jpg">
-              </div>
-              <h3 class="pad-bt15">Charles Cariño</h3>
-              <p>Born March 2, 1998. Currently studying computer science at the College of Computer Studies of Silliman University. Charles is currently the project designer of the College Inquiry Assessment.</p>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="wrap-item text-center">
-              <div class="item-img">
-                <img class="circle-profile"src="img/20621099_1621977307875142_7182323348226083932_n.jpg">
-              </div>
-              <h3 class="pad-bt15">Seth Cornelio</h3>
-              <p>Seth or Koya is resonsible for the database design and administraton. To learn more about him, add me on facebook: www.facebook.com/KuyaPres</p>
-            </div>
-          </div>
+
           <div class="col-md-3 col-sm-6 col-xs-12">
             <div class="wrap-item text-center">
               <div class="item-img">
                 <img class="circle-profile"src="img/16665275_1232677693452918_688165418877697166_o.jpg">
               </div>
               <h3 class="pad-bt15">Samuel Akinbode</h3>
-              <p>Born 15 June 1997. Currently studying Computer Science and Musical theatre in Silliman University.He is the System Analyst of this project</p>
+              <p>"It's okay to be average, <br>but it's not okay <br>to stay average." <br>- Sam</p>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="wrap-item text-center">
+              <div class="item-img">
+                <img class="circle-profile"src="img/13006598_999938766759664_6860126981732108419_n.jpg">
+              </div>
+              <h3 class="pad-bt15">Charles Cariño</h3>
+              <p>"Everything has a solution.<br> It's up to you to take the challenge or not." <br>- Charles</p>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="wrap-item text-center">
+              <div class="item-img">
+                <img class="circle-profile"src="img/20621099_1621977307875142_7182323348226083932_n.jpg">
+              </div>
+              <h3 class="pad-bt15">Seth Cornelio</h3>
+              <p>"I am a source code, <br>Life is the IDE and <br>God is the Programmer." <br>- Koya Seth"</p>
             </div>
           </div>
         </div>
