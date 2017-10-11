f0235387fe5966553cbaf1e044253e07836aa603
KoyaKuya <30855417+KuyaKoya@users.noreply.github.com>
2017-10-11 10:40:04 +0800

    Add files via upload
    changes in the about page

--- cia/about.php
+++ cia/about.php
@@ -1,8 +1,10 @@
+<!-- test -->
 <?php include "header.php"; ?>
 <section id="feature" class="section-padding wow fadeInUp delay-05s">
   <div class="col-md-12 text-center">
          <h2 class="service-title pad-bt15">About</h2>
-         <p class="sub-title pad-bt15">The team is composed of CCS6 students and are primarily amateurs<br>but are determined into completing this fun project.</p>
+         <p class="sub-title pad-bt15">The system aims to allow the students below college level to have an opportunity <br> to learn and explore the many possibilities of a college degree and career with accordance to their own special characteristics and skills.</p>
+         <p class="sub-title pad-bt15">The system also aims to allow the students to explore degrees which may be of interest to them <br> and also show the students the top five jobs related to the degree of their choice.</p>
          <hr class="bottom-line">
   </div>
   <div class="container-fluid cards-row">
@@ -15,20 +17,32 @@
          <img class="team-pic" src="img/20819343_851506305010941_8992152039636009533_o.jpg">
         <div class="caption">
           <h3>Derick Ho</h3>
-          <p class="card-description">A customized about us page design using bootstrap framework, HTML and CSS.</p>
-          <p class="social"><a href=""><i class="fa fa-facebook"></i></a></p>
+          <p class="card-description">Born April 28, 1994. Studying Computer Science in Silliman University, Derick originally invisioned this project that is why he was the manager of the team.</p>
+          <p class="social"><a target = "_blank" href="https://www.facebook.com/Doodyo123"><i class="fa fa-facebook"></i></a></p>
         </div>
       </div>
     </div>
 
     <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img class="back-img" src="img/mmi87kC.jpg" alt="about us page design using bootstrap">
+       <img class="team-pic" src="img/16665275_1232677693452918_688165418877697166_o.jpg">
+      <div class="caption">
+        <h3>Samuel Akinbode</h3>
+        <p class="card-description">Born 15th of June 1997. Studying Computer Science and Musical Theatre in Silliman University, Sam is the System Analyst of the College Inquiry Assessment System.</p>
+        <p class="social"><a target = "_blank" href="https://www.facebook.com/samuelakinbode"><i class="fa fa-facebook"></i></a>
+      </div>
+    </div>
+    </div>
+
+    <div class="col-sm-6 col-md-3">
       <div class="thumbnail">
         <img class="back-img" src="img/bright-green-leaves-in-a-random-pattern-creating-a-natural-background_a-G-4876026-14258389.jpg" alt="about us page design using bootstrap">
-         <img class="team-pic" src="img/13006598_999938766759664_6860126981732108419_n.jpg">
+         <img class="team-pic" src="img/11078111_882024558507435_8070152759120443374_n.jpg">
         <div class="caption">
           <h3>Charles Cariño</h3>
-          <p class="card-description">A customized about us page design using bootstrap framework, HTML and CSS.</p>
-          <p class="social"><a href=""><i class="fa fa-facebook"></i></a> <a href=""><i class="fa fa-github"></i></a></p>
+          <p class="card-description">Born March 2, 1998. Studying Computer Science in Silliman University, Charles is the project designer of the College Inquiry Assessment System.</p>
+          <p class="social"><a target = "_blank" href="https://www.facebook.com/charlesulquiorra4"><i class="fa fa-facebook"></i></a> <a target = "_blank" href="https://github.com/xdkappa"><i class="fa fa-github"></i></a></p>
         </div>
       </div>
     </div>
@@ -36,29 +50,15 @@
       <div class="col-sm-6 col-md-3">
       <div class="thumbnail">
         <img class="back-img" src="img/Best-Animated-Christian-Wallpaper-600x400.jpg" alt="about us page design using bootstrap">
-          <img class="team-pic" src="img/20621099_1621977307875142_7182323348226083932_n.jpg">
+          <img class="team-pic" src="img/15384534_1303225849750291_6703181460411652681_o.jpg">
         <div class="caption">
           <h3>Seth Cornelio</h3>
-          <p class="card-description">A customized about us page design using bootstrap framework, HTML and CSS.</p>
-          <p class="social"><a href=""><i class="fa fa-facebook"></i></a> <a href=""><i class="fa fa-twitter"></i></a> <a href=""><i class="fa fa-google-plus"></i></a></p>
+          <p class="card-description">Born 9th of November 1998. Taking up BS-Computer Science in Silliman University, Seth is the database administrator of the system.</p>
+          <p class="social"><a target = "_blank" href="https://www.facebook.com/KuyaPres"><i class="fa fa-facebook"></i></a> <a target = "_blank" href="https://www.instagram.com/system_sethings/?hl=en"><i class="fa fa-instagram"></i></a> <a target = "_blank" href="https://twitter.com/KoyaSeth"><i class="fa fa-twitter"></i></a></p>
         </div>
       </div>
     </div>
-
-      <div class="col-sm-6 col-md-3">
-      <div class="thumbnail">
-        <img class="back-img" src="img/mmi87kC.jpg" alt="about us page design using bootstrap">
-         <img class="team-pic" src="img/16665275_1232677693452918_688165418877697166_o.jpg">
-        <div class="caption">
-          <h3>Samuel Akinbode</h3>
-          <p class="card-description">A customized about us page design using bootstrap framework, HTML and CSS.</p>
-          <p class="social"><a href=""><i class="fa fa-facebook"></i></a> <a href=""><i class="fa fa-twitter"></i></a> <a href=""><i class="fa fa-linkedin"></i></a> <a href=""><i class="fa fa-google-plus"></i></a></p>
-        </div>
-      </div>
-    </div>
-
   </div>
-
   </div>
   </div>
 </section>
